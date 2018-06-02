162f945bc430a9a7d0b7f695503bd1d641f8ee4c
Ilgner de Oliveira Fagundes <ilgner.fagundes@izap.com.br>
2018-06-02 12:06:29 -0300

    env

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -151,7 +151,7 @@ class UserController extends Controller
 
         $this->logger->alert(
             "Teste",
-            $data
+            $request->getContent()
         );
 
         if(!is_array($data)){
